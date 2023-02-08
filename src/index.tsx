6e400e00ffe84439c348a835187b38b9fb3634d4
darmostuk98 <peterdarmostuk@gmail.com>
2023-02-08 14:08:48 +0100

    create elements with React.Fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// JSX Упрощенный вариант записи новых элементов в React
+const Title = () => {
+    return <h1>Hello React.js</h1>
+}
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+                sed.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+                vero.
+            </p>
+        </React.Fragment>
+    )
+}
 
-const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
-
-let a = 10
-let b = 'Hello World'
-
-const list1 = (
-    <ul>
-        <li>list item 1 {a + a}</li>
-        <li>list item 2 {b}</li>
-        <li>list item 3</li>
-    </ul>
-)
-
-//-------------------------------
-
-const list = React.createElement(
-    'ul',
-    null,
-    React.createElement('li', null, 'list Item 1'),
-    React.createElement('li', null, 'list Item 2'),
-    React.createElement('li', null, 'list Item 3')
-)
-const content = (
-    <div>
-        {h1}
-        {list}
-    </div>
-)
+function App() {
+    return (
+        <>
+            <Title />
+            <Content />
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{list1}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
