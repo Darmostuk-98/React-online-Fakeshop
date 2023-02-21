b971d05c3de8ee151748807a0efee8a88c2594e2
darmostuk98 <peterdarmostuk@gmail.com>
2023-02-21 14:15:44 +0100

    add smthng to files

---
+++ src/container/app/app.tsx
@@ -0,0 +1,15 @@
+import header from 'container/Header/header'
+import main from 'container/Main/main'
+import footer from 'container/Footer/footer'
+
+type Props = {}
+const app = (props: Props) => {
+    return (
+        <>
+            <header />
+            <main />
+            <footer />
+        </>
+    )
+}
+export default app
