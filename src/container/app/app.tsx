b512167012ce8473cadec0fdb9e85449e0e8ed45
darmostuk98 <peterdarmostuk@gmail.com>
2023-02-27 14:07:40 +0100

    create component from mui libriary

--- src/container/app/app.tsx
+++ src/container/app/app.tsx
@@ -1,11 +1,13 @@
 import Main from 'Container/Main/Main'
 import Footer from 'Container/Footer/Footer'
 import Header from 'Container/Header/Header'
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
 const App = (props: Props) => {
     return (
         <>
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
