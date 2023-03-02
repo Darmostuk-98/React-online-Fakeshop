b2018cfac5ed69afc32a3a8995fdcee38bcee9a2
darmostuk98 <peterdarmostuk@gmail.com>
2023-03-02 13:31:12 +0100

    add dedestruction in props and get them into div

--- src/components/ProductsList/ProductsListItem.tsx
+++ src/components/ProductsList/ProductsListItem.tsx
@@ -8,17 +8,15 @@ type Props = {
     price: number
 }
 
-const ProductsListItem = (props: Props) => {
+const ProductsListItem = ({ title, desc, type, capacity, price }: Props) => {
     return (
         <Card variant="outlined" className="product">
             <CardContent>
-                <div className="product-title">{props.title}</div>
-                <div className="product-desc"> {props.desc}</div>
-                <div className="product-features">Type: {props.type}</div>
-                <div className="product-features">
-                    Capacity: {props.capacity}
-                </div>
-                <div className="product-price">{props.price}$</div>
+                <div className="product-title">{title}</div>
+                <div className="product-desc"> {desc}</div>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacity: {capacity}</div>
+                <div className="product-price">{price}$</div>
             </CardContent>
             <CardActions className="btn-wrap">
                 <Button variant="outlined">Add to Cart</Button>
