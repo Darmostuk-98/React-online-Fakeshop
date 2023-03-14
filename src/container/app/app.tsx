c4d683788c8a3e8c504f7408a6b414e459370017
darmostuk98 <peterdarmostuk@gmail.com>
2023-03-14 16:33:33 +0100

    Добавили функцию при клике на кнопку добалвяем товары в корзину

--- src/container/app/app.tsx
+++ src/container/app/app.tsx
@@ -14,13 +14,24 @@ type CartDataProps = {
 
 const App = (props: Props) => {
     const [cartData, setCartdata] = useState<CartDataProps>({
-        totalCount: 10,
-        totalPrice: 100,
+        totalCount: 0,
+        totalPrice: 0,
     })
+
+    const addProductToCart = (count: number, price: number) => {
+        setCartdata((prevState) => ({
+            totalCount: prevState.totalCount + count,
+            totalPrice: prevState.totalPrice + count * price,
+        }))
+    }
+
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header cartData={cartData} />
+            <button onClick={() => addProductToCart(1, 100)}>
+                Добавить в корзину
+            </button>
             <Main />
             <Footer />
         </StyledEngineProvider>
